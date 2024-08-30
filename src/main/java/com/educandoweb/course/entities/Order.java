c646df4e57e04b47d382e8e1a9e454188e754f36
Alan Sumiya <alan.sumiya@gmail.com>
2024-08-30 10:43:04 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,7 +42,12 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order") //ele está mapeado na classe auxiliar como order
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)  //para que o id do pagamento seja
+	//o mesmo id do order
+	private Payment payment;
 
+	
 	public Order() {
 
 	}
@@ -90,6 +97,15 @@ public class Order implements Serializable {
 		if(orderStatus != null)
 		this.orderStatus = orderStatus.getCode();
 	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 
 	@Override
 	public int hashCode() {
